d53838af37692f05d6d167be288c36abc2bcc738
Nen <nen.work@protonmail.com>
2020-04-24 14:45:34 +0300

    logfiles change

--- app/sshclass.py
+++ app/sshclass.py
@@ -1,25 +1,11 @@
 import yaml
-from pexpect import pxssh
 import getpass
 import logging
 
-# TO DO file exist check try
-
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(name)-12s %(message)s',
-                    datefmt='%C%y-%m-%dT%H:%M:%S',#'%m-%d %H:%M',
-                    filename='/home/nen/expect-log.log',
-                    filemode='w')
-console = logging.StreamHandler()
-console.setLevel(logging.INFO)
-formatter = logging.Formatter('%(name)-12s %(message)s')
-console.setFormatter(formatter)
- # add the handler to the root logger
-logging.getLogger('').addHandler(console)   
-
+from pexpect import pxssh
 
 class device():
-    def __init__(self,name,username,password,getpassword,su,sugetpassword,use_sudo,interact,logfile,cmd):
+    def __init__(self,name,username,password,getpassword,su,sugetpassword,use_sudo,interact,cmd):
         self.name=name
         self.username=username
         self.password=str(password)
@@ -28,7 +14,6 @@ class device():
         self.sugetpassword=sugetpassword
         self.use_sudo=use_sudo
         self.interact=interact
-        self.logfile=logfile
         self.cmd=cmd
         self.logger = logging.getLogger(name)
         self.print_device()
@@ -56,14 +41,14 @@ class device():
     
     def print_device(self):
         try: 
-            print("name: {name}\nusername: {username}\npassword: {password}\ngetpassword: {getpassword}\nsu: {su}\nsugetpassword: {sugetpassword}\nuse_sudo: {use_sudo}\ninteract: {interact}\nlogfile: {logfile}\nCommands: {cmd}".format(name=self.name,
+            print("name: {name}\nusername: {username}\npassword: {password}\ngetpassword: {getpassword}\nsu: {su}\nsugetpassword: {sugetpassword}\nuse_sudo: {use_sudo}\ninteract: {interact}\nCommands: {cmd}".format(name=self.name,
                                                username=self.username,
                                                password=self.password,
                                                getpassword=self.getpassword,
                                                su=self.su,sugetpassword=self.sugetpassword,
                                                use_sudo=self.use_sudo,
                                                interact=self.interact,
-                                               logfile=self.logfile,cmd=self.cmd))
+                                               cmd=self.cmd))
         except Exception as e:
             print("Something went wrong with config.yaml: ",e) 
         #finally:
---
+++ main.py
@@ -0,0 +1,39 @@
+import yaml
+import logging
+
+from pexpect import pxssh
+from app import sshclass
+from time import gmtime, strftime
+
+
+with open('config.yml','r') as configfile:
+    config = yaml.safe_load(configfile)
+    devices = {}        
+    today = strftime("%y%m%d-%H%M%S", gmtime())
+    logfiledir = config['logfiledir']
+    # TO DO file exist check try
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(name)-12s %(message)s',
+                        datefmt='%C%y-%m-%dT%H:%M:%S',#'%m-%d %H:%M',
+                        filename=logfiledir+'/'+today+'.log',
+                        filemode='w')
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(name)-12s %(message)s')
+    console.setFormatter(formatter)
+    # add the handler to the root logger
+    logging.getLogger('').addHandler(console)   
+
+    for host in config['hosts']:      
+        devices[host['name']] = sshclass.device(name=host['name'],
+                                username=host['username'],
+                                password=host['password'],
+                                getpassword=host['getpassword'],
+                                su=host['su'],
+                                sugetpassword=host['sugetpassword'],
+                                use_sudo=host['use_sudo'],
+                                interact=host['interact'],
+                                cmd=host['cmd'])
+        
+
+
